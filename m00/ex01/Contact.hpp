b0ca1935f7cc12e76bb4a00cc4a734331ba58f49
0x0M4R <omar.abdalla123@gmail.com>
2023-03-03 15:13:34 +0400

    DONE

--- m00/ex01/Contact.hpp
+++ m00/ex01/Contact.hpp
@@ -6,7 +6,7 @@ class Contact
         int id;
         std::string details[5];
     public:
-        std::string getDetail(int i); // Accessor for the detail variable
+        std::string getDetail(int i);
         void setDetail(std::string detail, int i);
         int getId();
         void setId(int i);
