638c4f8bcf1dcdaede71ed09512132c49b264fa5
0x0M4R <omar.abdalla123@gmail.com>
2023-03-03 15:18:55 +0400

    doen forsure

--- m00/ex00/megaphone.cpp
+++ m00/ex00/megaphone.cpp
@@ -7,10 +7,9 @@ int main(int argc, char *argv[])
         std::string tmp = argv[i];
         for(size_t j = 0; j < tmp.length(); j++)
             std::cout << (char)toupper(tmp[j]);
-        if(tmp.length())
-            std::cout << "\n";
     }
     if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    std::cout << "\n";
     return(1);
 }
