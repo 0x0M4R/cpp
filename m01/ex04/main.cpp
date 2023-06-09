73f39ebfeaa6863c11e9225dede238534823e0e0
0x0M4R <omar.abdalla123@gmail.com>
2023-06-09 16:03:07 +0400

    mo1:ex04 complete

--- m01/ex04/main.cpp
+++ m01/ex04/main.cpp
@@ -2,28 +2,34 @@
 #include <string>
 #include <fstream>
 
-void	replace(std::string filename, std::string s1, std::string s2)
+int	replace( std::string filename, std::string s1, std::string s2 )
 {
-	std::ifstream input_file(filename);
-	if (!input_file) {
-		std::cerr << "Error: file cannot be opened" << std::endl;
-		exit(-1);
+	std::ifstream input_file( filename );
+	if ( !input_file )
+	{
+		std::cerr << "Error: file doesnt exist / cannot be opened" << std::endl;
+		return( -1 );
 	}
-	if (input_file.good()) {
-		if (input_file.peek() == std::ifstream::traits_type::eof())
+	if ( input_file.good() )
+	{
+		if ( input_file.peek() == EOF )
+		{
 				std::cout << "Error: file is empty" << std::endl;
-		else {
-			std::ofstream output_file(filename.append(".replace").data());
-			while (input_file.good() && output_file.good()) {
+				return ( -1 );
+		}
+		else
+		{
+			std::ofstream output_file( filename + ".replace" );
+			while ( input_file.good() && output_file.good() )
+			{
 				std::string line;
-				std::size_t word;
-				std::getline(input_file, line);
-				word = line.find(s1, 0);
-				while (word != std::string::npos)
+				std::getline( input_file, line );
+				std::size_t word = line.find( s1, 0 );
+				while ( word != std::string::npos )
 				{
-					line.erase(word, s1.length());
-					line.insert(word, s2);
-					word = line.find(s1, word);
+					line.erase( word, s1.length() );
+					line.insert( word, s2 );
+					word = line.find( s1, word );
 				}
 				output_file << line;
 				if (input_file.eof())
@@ -35,16 +41,17 @@ void	replace(std::string filename, std::string s1, std::string s2)
 		}
 	}
 	else
-		std::cout << "Error: " << strerror(errno) << std::endl;
+		std::cout << "Error: " << strerror( errno ) << std::endl;
+	return ( 0 );
 }
 
-int	main(int argc, char **argv)
+int	main( int argc, char **argv )
 {
-	if (argc == 4)
-		replace(argv[1], argv[2], argv[3]);
-	else {
-		std::cerr << "Error: ./replace <filename> <string1> <string2>" << std::endl;
-		exit(-1);
+	if ( argc == 4 )
+		return ( replace( argv[1], argv[2], argv[3] ) );
+	else
+	{
+		std::cerr << "Error: wrong parameters \nUsage: ./sed <filename> <s1> <s2>" << std::endl;
+		return( -1 );
 	}
-	return 0;
 }
