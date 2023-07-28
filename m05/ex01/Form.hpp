aa190fe85e48e6aa7bec60617abe88e949209b9d
0x0M4R <omar.abdalla123@gmail.com>
2023-07-28 14:14:12 +0400

    m02ex01 WIP

---
+++ m05/ex01/Form.hpp
@@ -0,0 +1,32 @@
+#ifndef Form_HPP
+#define Form_HPP
+#include "Bureaucrat.hpp"
+#include <iostream>
+class Form
+{
+	private:
+		const std::string name;
+        bool signed;
+        const int sign_grade;
+        const int exec_grade;
+	public:
+		Form();
+		Form( Form const & copy );
+		~Form();
+		Form& operator=( Form const &copy );
+        std::string getName( void ) const;
+        int getSigned( void ) const;
+        int getSignGrade( void ) const;
+        int getExecGrade( void ) const;
+        void beSigned( Bureaucrat B ); //reverse 
+        // class GradeTooHighException : public std::exception
+		// {
+		// 	virtual const char* what(void) const throw();
+		// };
+		// class GradeTooLowException : public std::exception
+		// {
+		// 	virtual const char* what(void) const throw();
+		// };
+};
+std::ostream& operator<<( std::ostream& os, Form const &rhs );
+#endif
