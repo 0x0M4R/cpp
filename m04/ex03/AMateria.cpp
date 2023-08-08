d2093d20eb6b0cd9f8bca2cbf4f8fcb79a887fbd
0x0M4R <omar.abdalla123@gmail.com>
2023-08-08 19:19:22 +0400

    cpp04ex03

---
+++ m04/ex03/AMateria.cpp
@@ -0,0 +1,27 @@
+#include "AMateria.cpp"
+AMateria::AMateria()
+{
+    
+}
+AMateria::AMateria(std::string const & type)
+{
+    
+}
+
+AMateria~AMateria()
+{
+    
+}
+AMateria::AMateria(Animal const & copy)
+{
+    
+}
+AMateria &AMateria::operator=( Animal const &copy )
+{
+    
+}
+
+std::string const & AMateria::getType() const
+{
+    
+}
