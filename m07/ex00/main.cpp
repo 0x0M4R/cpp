00976673fb0437b0099a51e180cd490027c387c1
0x0M4R <omar.abdalla123@gmail.com>
2023-08-11 15:12:48 +0400

    something wrong

--- m07/ex00/main.cpp
+++ m07/ex00/main.cpp
@@ -1,5 +1,28 @@
 #include "whatever.hpp"
-
+// template <class T>
+class Data 
+{
+    private :
+        int d;
+    public:
+        Data(void): d(0) {}
+        Data(int d): d(d) {}
+        Data &operater=( Data &c) {
+            d=c.d;
+            return (*this);
+        }
+        bool operator<=(Data const &c) const {
+            return (d<=c.d);
+        }
+        bool operator>=(Data const &c) const {
+            return (d>=c.d);
+        }
+        int getd() const { return n;}
+};
+std::ostream &operator<<( std::ostream & os, Data &c)
+{
+    return (os << c.getd());
+}
 int main()
 {
     int i = 2;
